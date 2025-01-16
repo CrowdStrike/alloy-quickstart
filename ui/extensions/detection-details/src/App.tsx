b32ffcd4e3be91a7e0fca98ec03694472be71f19
Evan Stoner <evan.stoner@crowdstrike.com>
2025-01-16 16:23:38 -0500

    initial commit

---
+++ ui/extensions/detection-details/src/App.tsx
@@ -0,0 +1,9 @@
+import { Panel, Title } from "@patternfly/react-core";
+
+export default function App() {
+  return (
+    <Panel>
+      <Title headingLevel="h1">Hello from Patternfly</Title>
+    </Panel>
+  );
+}
