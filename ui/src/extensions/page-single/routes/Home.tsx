121ea51ad60b0a156382cb9f7273ca3feb93b0b5
Evan Stoner <evan.stoner@crowdstrike.com>
2025-02-11 13:14:32 -0500

    clean up unnecessary changes

--- ui/src/extensions/page-single/routes/Home.tsx
+++
@@ -1,9 +0,0 @@
-import { PageSection, Title } from "@patternfly/react-core";
-
-export default function Home() {
-  return (
-    <PageSection>
-      <Title headingLevel="h3">Home</Title>
-    </PageSection>
-  );
-}
--- ui/src/extensions/page-single/routes/Settings.tsx
+++
@@ -1,9 +0,0 @@
-import { PageSection, Title } from "@patternfly/react-core";
-
-export default function Settings() {
-  return (
-    <PageSection>
-      <Title headingLevel="h3">Settings</Title>
-    </PageSection>
-  );
-}
