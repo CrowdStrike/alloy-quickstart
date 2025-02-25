5e8690a2489ead7925e40310ab9426b2a6bcae3d
Evan Stoner <evan.stoner@crowdstrike.com>
2025-02-25 10:49:06 -0500

    feat: add gh actions, dependabot, and fix alloy-react imports

--- ui/src/apps/page-single/index.tsx
+++ ui/src/apps/page-single/index.tsx
@@ -1,4 +1,4 @@
-import { FoundryProvider } from "alloy-react";
+import { FoundryProvider } from "@crowdstrike/alloy-react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
--- ui/src/apps/pages/App.tsx
+++ ui/src/apps/pages/App.tsx
@@ -1,5 +1,5 @@
+import { ConsolePage, useFoundry } from "@crowdstrike/alloy-react";
 import "@patternfly/react-core/dist/styles/base.css";
-import { ConsolePage, useFoundry } from "alloy-react";
 import Home from "./routes/Home";
 import Settings from "./routes/Settings";
 
