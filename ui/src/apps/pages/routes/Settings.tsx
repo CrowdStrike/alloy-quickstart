ed5cab4e6d319e4b5c53fad21be67b12cb9f2566
Evan Stoner <evan.stoner@crowdstrike.com>
2025-03-18 12:35:35 -0400

    use alloy-react CollectionEditor

--- ui/src/apps/pages/routes/Settings.tsx
+++ ui/src/apps/pages/routes/Settings.tsx
@@ -1,11 +1,20 @@
+import { CollectionEditor } from "@crowdstrike/alloy-react";
 import { PageSection, Title } from "@patternfly/react-core";
-import CollectionEditor from "../../../components/CollectionEditor";
 
 export default function Settings() {
   return (
     <PageSection>
       <Title headingLevel="h3">Settings</Title>
-      <CollectionEditor />
+      <CollectionEditor
+        collectionNameDefault="config"
+        collectionNameEditable={false}
+        objectNameDefault="default"
+        objectNameEditable={false}
+        loadObjectValue={true}
+        loadButtonVisible={false}
+        deleteButtonVisible={false}
+        objectValueDefault={{ special_key: "PLACEHOLDER" }}
+      />
     </PageSection>
   );
 }
